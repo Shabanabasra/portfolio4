8d2531469f3e2083b4e96929b577bdfbee7b07c4
Shabanabasra <160415880+Shabanabasra@users.noreply.github.com>
2024-11-22 04:54:26 -0800

    push my Assignment o github

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div id="about" className="about-container" >
             <h2 className="about-heading" data-aos="zoom-in-up">About Me</h2>
-            <p className="about-text" data-aos="fade-up">
+            <p className="about-text" data-aos="zoom-in-up">
                 I am a student at GIAIC, pursuing a course in Artificial Intelligence, Web 3.0, & Metaverse. 
                 Passionate about technology, I am dedicated to continuously learning new skills to stay updated with the latest innovations.
             </p>
